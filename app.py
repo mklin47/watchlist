d53ded1a9d55b74318c9889565a134ed01b78a89
mklin <374031566@qq.com>
2019-04-19 11:44:27 +0800

    add base template and error template

--- app.py
+++ app.py
@@ -75,6 +75,18 @@ def forge():
 
 @app.route("/")
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
+
+
+# 错误处理函数
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+
+# 模版上下文
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于return {'user': user}
